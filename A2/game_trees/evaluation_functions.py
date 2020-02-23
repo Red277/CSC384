c23401f3eb11078871f512762aaf9e247ecc179b
galegoer <eric.m.galego@gmail.com>
2020-02-23 14:03:33 -0500

    finished q5

--- A2/game_trees/evaluation_functions.py
+++ A2/game_trees/evaluation_functions.py
@@ -18,6 +18,14 @@ from state import *
 class EvaluationFunctions:
 
   @staticmethod
+  def manhattan_heuristic(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+  @staticmethod
+  def euclidean_heuristic(p1, p2):
+    return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5  
+
+  @staticmethod
   def score_evaluation(state):
     return state.get_score()
   
@@ -25,12 +33,64 @@ class EvaluationFunctions:
   def box_evaluation(state):
     #Question 4, your box evaluation solution goes here
     #Returns a numeric value evaluating the given state where the larger the better
-    return 0
-    #raise NotImplementedError("Box Evaluation not implemented")
+    #return 0
+    raise NotImplementedError("Box Evaluation not implemented")
 
   @staticmethod
   def points_evaluation(state):
     #Question 5, your points evaluation solution goes here
     #Returns a numeric value evaluating the given state where the larger the better
-    return 0
-    #raise NotImplementedError("Points Evaluation not implemented")
+    for pos in state.get_switches():
+      #only one switch so simply set it to switch_pos
+      switch_pos = pos
+    player_pos = state.get_player_position()
+    if state.get_obtained_points() >= 5: 
+      #Case of only need to hit switch compare switch and player position
+      return 1/EvaluationFunctions.euclidean_heuristic(player_pos, switch_pos)
+    distances = []
+    total_dis = 0
+    point_to_switch = 0
+    player_to_point = 0
+    player_pos = state.get_player_position()
+    temp_remain = state.get_remaining_points()[:] #copy remaining points so we can delete from copy
+    
+    for k in range(5 - state.get_obtained_points(), 0, -1):
+      if k == 1: #IF ONLY ONE POINT IS LEFT TO OBTAIN
+        for i in range(0, len(temp_remain)):
+          point_pos = temp_remain[i]
+          player_to_point = EvaluationFunctions.euclidean_heuristic(player_pos, point_pos) * 1.5 
+          #Points evaluate smaller here because we do inverse later making them bigger (need point before we get to switch)
+          point_to_switch = EvaluationFunctions.euclidean_heuristic(point_pos, switch_pos) * 3 
+          #want combined distance between player and point, point and switch
+          sum_both = ((player_to_point) + (point_to_switch))
+          distances.append(sum_both)
+        for enemy in state.get_enemies():
+          total_dis += EvaluationFunctions.euclidean_heuristic(player_pos, enemy)
+        #Apparently you have to check this because sometimes there aren't enough points on the board 
+        #to obtain the boots so you could have an empty distances and the check loops through the 5 points
+        #For example the first test case in Q5 has only 3 points and 0 obtained so it just ignores the fact
+        #that you need 5 points and says best state is when there is no points left and you're at the switch
+        #even though you don't have 5 points
+        if distances != []:
+          distances.sort() 
+          total_dis += 1/distances[0] #smallest distance is most optimal do inverse because bigger better
+          #reset distances
+          distances = []
+      else:
+        for i in range(0, len(temp_remain)):
+          point_pos = temp_remain[i]
+          player_to_point = (EvaluationFunctions.euclidean_heuristic(player_pos, point_pos), point_pos)
+          distances.append(player_to_point)
+        #find and add the shortest distance between points and player
+        for enemy in state.get_enemies():
+          total_dis += EvaluationFunctions.euclidean_heuristic(player_pos, enemy)
+        if distances != []:
+          distances.sort()
+          temp_remain.remove(distances[0][1]) #remove point because we theoretically got the point
+          player_pos = distances[0][1] #update player_pos because we theoretically moved them
+          #need to do the inverse because smaller is better here but overall want bigger to be better
+          #multiply by 1.5 because points are more important than enemies
+          total_dis += 1/(distances[0][0] * 1.5)
+          #reset distances
+          distances = []
+    return total_dis    
